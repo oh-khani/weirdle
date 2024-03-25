77229679a0fb3111351a38ff16c7997515c37296
Evan BERTIN <evan.bertin@etu.univ-lyon1.fr>
2024-03-25 08:34:16 +0100

    correction lien

--- src/assets/navbar.php
+++ src/assets/navbar.php
@@ -8,10 +8,10 @@
      
     foreach ($pages as $url => $title) {
         $active = '';
-        if ($_SERVER['REQUEST_URI'] === "/weirdle/src/pages/$url") {
+        if ($_SERVER['REQUEST_URI'] === "/~p2301285/weirdle/src/pages/$url") {
             $active = 'class="active"';
         } 
-        echo "<li><a href=/weirdle/src/pages/$url $active>$title</a></li>";
+        echo "<li><a href=/~p2301285/weirdle/src/pages/$url $active>$title</a></li>";
     } ?>
     </ul>
 </nav>
