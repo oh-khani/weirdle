9a5bc1d19c1a642df53aac16550391f432f06f5b
mathymm26 <tim.bertholet26@gmail.com>
2024-05-23 11:04:49 +0200

    choix du mode de jeux

--- src/game/Game.js
+++ src/game/Game.js
@@ -3,5 +3,14 @@ import { Chronometre } from './game_modes/Chronometre.js';
 import { Invisible } from './game_modes/Invisible.js'
 
 const wordle = new Wordle();
-wordle.setMode(new Invisible(wordle));
+let modeChoisis = "invisible";
+
+switch (modeChoisis) 
+{
+    case "invisible": wordle.setMode(new Invisible(wordle)); break;
+    case "chronometre": wordle.setMode(new Chronometre(wordle)); break;
+    case "difficile": wordle.setMode(new Chronometre(wordle)); break;
+    default: break;
+}
+
 wordle.startGame();
