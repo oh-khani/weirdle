5519d58258b0da8c2e8f842ecd1c160d0d0d004e
KHEEREESANTIKUL KHANISON p2311069 <khanison.kheereesantikul@etu.univ-lyon1.fr>
2024-06-06 21:43:38 +0200

    ajustement

--- index.php
+++ index.php
@@ -1,21 +1,8 @@
-<header class="site-header">
 <?php 
 require_once 'src/assets/header.php';
 ?>
-</header>
 <head>
     <style>
-        /* Votre CSS pour l'écran d'accueil */
-        .site-header {
-            visibility: hidden;
-            position: fixed; /* Fixe le header en haut de la page */
-            top: 0; /* Positionne le header en haut de la page */
-            left: 0; /* Alignement à gauche */
-            width: 100%; /* Largeur pleine de la page */
-            padding: 20px; /* Espacement interne */
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Ombre */
-            z-index: 1000; /* Pour s'assurer que le header est au-dessus du contenu */
-        }
         .container {
             display: flex;
             flex-direction: column;
@@ -61,7 +48,7 @@ require_once 'src/assets/header.php';
 <div class="container">
     <h1 class="title">Weirdle</h1>
     <div class="button-container" style="display: flex; justify-content: center;">
-        <a href="LeGame.php" class="play-button">Jouer</a>
+        <a href="weirdle.php" class="play-button">Jouer</a>
     </div>
     <div class="button-container" style="display: flex; justify-content: center;">
         <a href="src/pages/connexion.php" class="login-button">Connexion</a>
