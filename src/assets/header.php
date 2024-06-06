b64321b4377f5948bf327af72b2c579220bb46b9
KHEEREESANTIKUL KHANISON p2311069 <khanison.kheereesantikul@etu.univ-lyon1.fr>
2024-06-06 21:37:44 +0200

    liens non draggable

--- src/assets/header.php
+++ src/assets/header.php
@@ -61,13 +61,13 @@
                 if ($_SERVER['REQUEST_URI'] === "/~p2301285/weirdle/src/pages/$url") {
                     $active = 'class="active"';
                 } 
-                echo "<li><a href=/~p2301285/weirdle/src/pages/$url $active>$title</a></li>";
+                echo "<li><a draggable='false' href=/~p2301285/weirdle/src/pages/$url $active>$title</a></li>";
             } ?>
             </ul>
         </div>
 
         <div class="navbar-center">
-            <a id="titre" href="/~p2301285/weirdle/"><h1 class="dm-sans-titre">Weirdle</h1></a>
+            <a draggable='false' id="titre" href="/~p2301285/weirdle/"><h1 class="dm-sans-titre">Weirdle</h1></a>
         </div>
         
 
@@ -75,13 +75,13 @@
         <?php
         if (isset($_SESSION['user'])) {
             $active = Active('profil.php');
-            echo "<li><a href='/~p2301285/weirdle/src/pages/profil.php' $active>".strtoupper($_SESSION['user']['pseudo'])."$notif</a></li>";
-            echo "<li><a href='/~p2301285/weirdle/src/pages/deconnexion.php'>Déconnexion</a></li>";
+            echo "<li><a draggable='false' href='/~p2301285/weirdle/src/pages/profil.php' $active>".strtoupper($_SESSION['user']['pseudo'])."$notif</a></li>";
+            echo "<li><a draggable='false' href='/~p2301285/weirdle/src/pages/deconnexion.php'>Déconnexion</a></li>";
         } else {
             $active = Active('inscription.php');
-            echo "<li><a href='/~p2301285/weirdle/src/pages/inscription.php' $active>Inscription</a></li>";
+            echo "<li><a draggable='false' href='/~p2301285/weirdle/src/pages/inscription.php' $active>Inscription</a></li>";
             $active = Active('connexion.php');
-            echo "<li><a href='/~p2301285/weirdle/src/pages/connexion.php' $active>Connexion</a></li>";
+            echo "<li><a draggable='false'href='/~p2301285/weirdle/src/pages/connexion.php' $active>Connexion</a></li>";
         }
         ?>
     </ul>
